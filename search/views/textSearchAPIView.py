1714550bae41715c8668f3af0c6da62fd54b7852
rayleigh <rayleigh190@gmail.com>
2023-09-14 21:49:52 +0900

    fix: 네이버 검색 링크 주소 모바일론 수정

--- search/views/textSearchAPIView.py
+++ search/views/textSearchAPIView.py
@@ -12,7 +12,7 @@ class TextSearchView(APIView):
     encoded_item_name = urllib.parse.quote(item_name)
 
     # naver_list_link = "https://msearch.shopping.naver.com/search/all?frm=NVSHMDL&origQuery="+encoded_item_name+"&pagingIndex=1&pagingSize=40&productSet=model&query="+encoded_item_name+"&sort=rel&viewType=lst"
-    naver_list_link = "https://search.shopping.naver.com/search/all?query="+encoded_item_name+"&cat_id=&frm=NVSHATC"
+    naver_list_link = "https://msearch.shopping.naver.com/search/all?query="+encoded_item_name+"&cat_id=&frm=NVSHATC"
 
     enuri_list_link = "https://m.enuri.com/m/search.jsp?keyword="+encoded_item_name
 
